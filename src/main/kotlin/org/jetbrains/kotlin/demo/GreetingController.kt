277d081405fbb030a16754ee4c40ee3b416f95bd
Devin Brown <devin.g.brown@gmail.com>
2017-12-05 19:48:37 -0700

    wip

--- src/main/kotlin/org/jetbrains/kotlin/demo/GreetingController.kt
+++ src/main/kotlin/org/jetbrains/kotlin/demo/GreetingController.kt
@@ -11,8 +11,6 @@ class GreetingController {
     val counter = AtomicLong()
 
     @GetMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-            Greeting(counter.incrementAndGet(), "Hello, $name")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) = Greeting(counter.incrementAndGet(), "Hello, $name")
 
 }
-
